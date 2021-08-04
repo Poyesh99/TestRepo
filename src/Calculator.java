aea8f0c6c335087fcae3db7c5c7ea10766af9797
Poyesh <prefahi99@gmail.com>
2021-08-04 17:52:27 -0400

    first commit

---
+++ src/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 11;
+        int b = 6;
+        System.out.println("a + b");
+        System.out.println("a - b");
+        System.out.println("a * b");
+        System.out.println("a / b");
+
+    }
